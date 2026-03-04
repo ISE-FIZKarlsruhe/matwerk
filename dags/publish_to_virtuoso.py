e68b39a05fab1978302908593bb65ef2956e439d
ebrahimnorouzi <45043740+ebrahimnorouzi@users.noreply.github.com>
2026-03-04 10:17:27 +0100

    dataportal is not finished

--- dags/publish_to_virtuoso.py
+++ dags/publish_to_virtuoso.py
@@ -33,7 +33,7 @@ PUBLISH_SOURCES = [
     ("spreadsheets_validated", "matwerk_last_successful_validated_run", "spreadsheets_merged_for_validation.ttl"),
     ("zenodo_validated", "matwerk_last_successful_harvester_zenodo_run", "zenodo_merged_for_validation.ttl"),
     ("endpoints_validated", "matwerk_last_successful_harvester_endpoints_run", "endpoints_merged_for_validation.ttl"),
-    ("pmd_validated", "matwerk_last_successful_harvester_pmd_run", "pmd_merged_for_validation.ttl"),
+    #("pmd_validated", "matwerk_last_successful_harvester_pmd_run", "pmd_merged_for_validation.ttl"),
 ]
 
 
